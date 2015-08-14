aa1ac34499d64b3302d8a13f4a54a82f7934113f
ming <ming@stash>
2015-08-14 17:25:42 +0800

    add cart

--- depot/test/models/product_test.rb
+++ depot/test/models/product_test.rb
@@ -4,4 +4,14 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+fixtures :products
+test "product is not valid without a unique title - i18n" do
+product = Product.new(title: products(:ruby).title,
+description: "yyy",
+price: 1,
+image_url: "fred.gif")
+assert !product.save
+assert_equal I18n.translate('activerecord.errors.messages.taken'),
+product.errors[:title].join('; ')
+end
 end
